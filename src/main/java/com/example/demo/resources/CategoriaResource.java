116f3d9fe589cd606c642d5f839bdfde6e62ee43
Eder Dias <eder.odias@hotmail.com>
2019-07-07 22:20:45 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -15,7 +15,7 @@ public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat1 = new Categoria(1,"Informática");
 		Categoria cat2 = new Categoria(2,"Escritório");
 		List<Categoria> lista = new ArrayList<>();
 		lista.add(cat1);
