b36d4079ababcd52653d9c56d3463f62f47e3442
Eder Dias <eder.odias@hotmail.com>
2019-07-31 12:04:32 -0300

    ItemPedido e ItemPedidoPK (Aula 26)

---
+++ src/main/java/com/example/demo/domain/ItemPedidoPK.java
@@ -0,0 +1,33 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class ItemPedidoPK implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name="produtoID")
+	private Produto produto;
+	@ManyToOne
+	@JoinColumn(name="pedidoID")
+	private Pedido pedido;
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	
+}
--- src/main/java/com/example/demo/domain/Pedido.java
+++ src/main/java/com/example/demo/domain/Pedido.java
@@ -1,7 +1,11 @@
 package com.example.demo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -31,7 +36,10 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name="EnderecoDeEntregaID")
 	private Endereco enderecoDeEntrega;
-
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
+	
 	public Pedido() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -44,7 +52,13 @@ public class Pedido implements Serializable{
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
-
+	public List<Produto> getProdutos(){
+		List<Produto> lista = new ArrayList<Produto>();
+		for(ItemPedido x : itens) {
+			lista.add(x.getProduto());
+		}
+		return lista;
+	}
 	public Integer getId() {
 		return id;
 	}
@@ -83,6 +97,39 @@ public class Pedido implements Serializable{
 
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	
