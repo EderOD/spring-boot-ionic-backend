6af17b33184bebf2232a033fe6067c00b4912d53
Eder Dias <eder.odias@hotmail.com>
2020-01-03 15:30:59 -0300

    Personalizando o retorno da validação (aula 37)

---
+++ src/main/java/com/example/demo/resources/exception/ValidationError.java
@@ -0,0 +1,25 @@
+package com.example.demo.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<FieldMessage>();
+	
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName,message));
+	}
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
