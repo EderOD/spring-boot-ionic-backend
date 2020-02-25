83c4f7c48e49d24e80a06495abb561b83b22224e
Eder Dias <eder.odias@hotmail.com>
2020-02-24 22:47:20 -0300

    Validação customizada: email não repetido na atualização de cliente (aula 45)

--- src/main/java/com/example/demo/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/example/demo/services/validation/ClienteInsertValidator.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.demo.domain.Cliente;
 import com.example.demo.domain.enums.TipoCliente;
 import com.example.demo.dto.ClienteNewDTO;
@@ -14,7 +16,9 @@ import com.example.demo.resources.exception.FieldMessage;
 import com.example.demo.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
 	private ClienteRepository repo;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -22,21 +26,19 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-		
+
 		if (objDto.getTipo() == null) {
-			list.add(new FieldMessage("tipo","Tipo não pode ser nulo"));
+			list.add(new FieldMessage("tipo", "Tipo não pode ser nulo"));
 		}
-		if(objDto.getTipo() > 3) {
-			list.add(new FieldMessage("tipo","Tipo não pode ser maior que 2"));
-		}
-		else if(objDto.getTipo() == TipoCliente.PESSOAFISICA.getCod() && !BR.isValidCPF(objDto.getcpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj","Número de CPF inválido!"));
-		}
-		else if(objDto.getTipo() == TipoCliente.PESSOAJURIDICA.getCod() && !BR.isValidCNPJ(objDto.getcpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj","Número de CNPJ inválido!"));
+		if (objDto.getTipo() > 3) {
+			list.add(new FieldMessage("tipo", "Tipo não pode ser maior que 2"));
+		} else if (objDto.getTipo() == TipoCliente.PESSOAFISICA.getCod() && !BR.isValidCPF(objDto.getcpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "Número de CPF inválido!"));
+		} else if (objDto.getTipo() == TipoCliente.PESSOAJURIDICA.getCod() && !BR.isValidCNPJ(objDto.getcpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "Número de CNPJ inválido!"));
 		}
 		Cliente aux = repo.findByEmail(objDto.getEmail());
-		if(aux != null) {
+		if (aux != null) {
 			list.add(new FieldMessage("email", "Email já existente!"));
 		}
 		for (FieldMessage e : list) {
